70313d75028ea067be02a0471443cf3bd4decd28
Thales Macedo Garitezi <thalesmg@gmail.com>
2021-11-19 09:03:29 -0300

    genesis

---
+++ files/tuning-loadgen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+
+{
+  sysctl -w net.ipv4.ip_local_port_range="1025 65535"
+  echo 1000000 > /proc/sys/fs/nr_open
+  ulimit -n 100000
+
+  # William's config
+  sysctl -w net.core.rmem_default=262144000
+  sysctl -w net.core.wmem_default=262144000
+  sysctl -w net.core.rmem_max=262144000
+  sysctl -w net.core.wmem_max=262144000
+  sysctl -w net.ipv4.tcp_mem="378150000  504200000  756300000"
+}
