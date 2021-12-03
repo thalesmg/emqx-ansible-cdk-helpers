2d561de4bd7d68d246aca94fba55390761cdc72b
Thales Macedo Garitezi <thalesmg@gmail.com>
2021-12-03 13:00:46 -0300

    increase the number of lg procs

--- templates/sub-unique-test.sh
+++ templates/sub-unique-test.sh
@@ -6,12 +6,10 @@ LG_NUM=$1
 OUTPUT_RECORDING=$2
 
 TARGET="emqx-$(( $LG_NUM % {{ groups['emqx'] | length }} )).int.{{ emqx_cluster_name }}"
-ipaddrs=$(ip addr | grep -o '192.*/32' | sed 's#/32##g' | paste -s -d , -)
-ulimit -n 1000000
 
 cd /root/emqtt-bench/
 
 {
   env TZ={{ script_timezone }} date
-  time ./emqtt_bench sub -c {{ emqtt_bench_number_of_connections }} -i {{ emqtt_bench_interval }} -x {{ emqtt_bench_session_expiry_interval }} -t 'bench/%c/#' -h $TARGET --ifaddr $ipaddrs 2>&1
+  time ./with-ipaddrs.sh ./emqtt_bench sub -c {{ emqtt_bench_number_of_connections }} -i {{ emqtt_bench_interval }} -x {{ emqtt_bench_session_expiry_interval }} -t 'bench/%c/#' -h $TARGET 2>&1
 } > "/tmp/$OUTPUT_RECORDING"
