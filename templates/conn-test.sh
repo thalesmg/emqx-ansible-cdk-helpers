54d40724dcd4e18fa620d23595f45107d9f70e15
Thales Macedo Garitezi <thalesmg@gmail.com>
2021-12-08 17:48:36 -0300

    lowmem mode for conn-test and loadbalancing with nprocs

--- templates/conn-test.sh
+++ templates/conn-test.sh
@@ -11,5 +11,5 @@ cd /root/emqtt-bench/
 
 {
   env TZ={{ script_timezone }} date
-  time ./with-ipaddrs.sh ./emqtt_bench conn -c {{ emqtt_bench_number_of_connections }} -i {{ emqtt_bench_interval }} -h $TARGET 2>&1
+  time ./with-ipaddrs.sh ./emqtt_bench conn -c {{ emqtt_bench_number_of_connections }} -i {{ emqtt_bench_interval }} -h $TARGET {{ "--lowmem" if (emqtt_bench_lowmem_mode | default(False) | bool) else "" }} 2>&1
 } > "/tmp/$OUTPUT_RECORDING"
