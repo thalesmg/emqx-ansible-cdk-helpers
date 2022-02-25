578ed4ef6bc9da28f7f3b68cb3ac01cbdcb3d2fe
Thales Macedo Garitezi <thalesmg@gmail.com>
2022-02-25 13:51:39 -0300

    add new test and tag log collection

---
+++ templates/pub-unique-simple-test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eu
+
+LG_NUM=$1
+OUTPUT_RECORDING=$2
+
+{% if groups['replicants'] %}
+# Note: targeting only replicants when they exist
+TARGET="emqx-$(( {{ emqx_num_cores }} + $LG_NUM % {{ groups['replicants'] | length }} )).int.{{ emqx_cluster_name }}"
+{% else %}
+TARGET="emqx-$(( $LG_NUM % {{ groups['emqx'] | length }} )).int.{{ emqx_cluster_name }}"
+{% endif %}
+
+START_NUM=$(( $LG_NUM * {{ emqtt_bench_number_of_connections }} ))
+
+cd /root/emqtt-bench/
+
+{
+  env TZ={{ script_timezone }} date
+  time ./with-ipaddrs.sh ./emqtt_bench pub \
+       -c {{ emqtt_bench_number_of_connections }} \
+       -i {{ emqtt_bench_interval }} \
+       -I {{ emqtt_bench_pub_interval }} \
+       -x {{ emqtt_bench_session_expiry_interval }} \
+       -t 'bench/%c/test' \
+       {{ "--lowmem" if (emqtt_bench_lowmem_mode | default(False) | bool) else "" }} \
+       -n "$START_NUM" \
+       {{ "--prefix \"" + emqtt_bench_prefix + "\"" if emqtt_bench_prefix is defined else "" }} \
+       {{ "--shortids" if (emqtt_bench_shortids | default(False) | bool) else "" }} \
+       -h $TARGET 2>&1
+} > "/tmp/$OUTPUT_RECORDING"
