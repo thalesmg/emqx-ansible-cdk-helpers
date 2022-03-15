0642e62676920d399339dcb8eb4e5abd6e620620
Thales Macedo Garitezi <thalesmg@gmail.com>
2022-03-15 18:00:28 -0300

    fix: rm packages...

--- update-emqx.sh
+++ update-emqx.sh
@@ -8,11 +8,14 @@ rm mix.lock rebar.lock || true
 
 EMQX_BUILDER_IMAGE="ghcr.io/emqx/emqx-builder/5.0-8:1.13.3-24.2.1-1-ubuntu20.04"
 EMQX_BRANCH="perf-test-mnesia-post-commit-hook-patches1"
+# EMQX_BRANCH="perf-test-master"
 
 git checkout "${EMQX_BRANCH}"
 git pull -r
 
-rm ./_packages || true
+rm -rf ./_packages || true
+
+make clean-all
 
 docker run --rm -i \
            -v "$PWD":/emqx \
