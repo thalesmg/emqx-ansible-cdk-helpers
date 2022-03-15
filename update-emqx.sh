1f59f2af9c884613f9b93eb0f8db85ae5f9a1672
Thales Macedo Garitezi <thalesmg@gmail.com>
2022-03-15 11:20:32 -0300

    update emqx script

---
+++ update-emqx.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -exu
+
+cd /root/emqx
+
+rm mix.lock rebar.lock || true
+
+EMQX_BUILDER_IMAGE="ghcr.io/emqx/emqx-builder/5.0-8:1.13.3-24.2.1-1-ubuntu20.04"
+EMQX_BRANCH="perf-test-mnesia-post-commit-hook-patches1"
+
+git checkout "${EMQX_BRANCH}"
+git pull -r
+
+rm ./_packages || true
+
+docker run --rm -i \
+           -v "$PWD":/emqx \
+           -w /emqx \
+           -e EMQX_NAME="emqx" \
+           -e HOME="/root" \
+           "$EMQX_BUILDER_IMAGE" \
+           bash -c "make emqx-elixir-pkg"
+
+yes N | dpkg -i ./_packages/emqx/*.deb
