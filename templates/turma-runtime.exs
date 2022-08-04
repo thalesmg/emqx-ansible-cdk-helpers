424359b6bcfb799e0c7e45e0b67d223c0c687a7d
Thales Macedo Garitezi <thalesmg@gmail.com>
2022-08-04 10:04:24 -0300

    update turma for dealer-router

--- templates/turma-runtime.exs
+++ templates/turma-runtime.exs
@@ -2,31 +2,29 @@ import Config
 
 config Turma.Decurio,
   inventory: %{
-    {% for replicant in (groups["replicants"] | default([]))  %}
-      "{{ replicant }}:19876" => ["replicant", "emqx"],
-    {% endfor %}
-    {% for core in (groups["cores"] | default([]))  %}
-      "{{ core }}:19876" => ["core", "emqx"],
-    {% endfor %}
-    {% for lg in (groups["loadgen"] | default([]))  %}
-      "{{ lg }}:19876" => ["loadgen"],
-    {% endfor %}
+    "emqx" => [
+      {% for emqx in (groups["emqx"] | default([]))  %}
+      "{{ emqx }}:19876",
+      {% endfor %}
+    ],
+    "cores" => [
+      {% for core in (groups["cores"] | default([]))  %}
+      "{{ core }}:19876",
+      {% endfor %}
+    ],
+    "replicants" => [
+      {% for replicant in (groups["replicants"] | default([]))  %}
+      "{{ replicant }}:19876",
+      {% endfor %}
+    ],
+    "loadgens" => [
+      {% for loadgen in (groups["loadgen"] | default([]))  %}
+      "{{ loadgen }}:19876",
+      {% endfor %}
+    ],
   },
   name: "decurio-{{ inventory_hostname_short }}"
 
 config Turma.Legionarius,
   bind: {"{{ ansible_default_ipv4.address }}", 19876},
-  subscriptions: [
-    {% if "loadgen" in group_names %}
-    "loadgen",
-    {% endif %}
-    {% if "cores" in group_names %}
-    "core",
-    {% endif %}
-    {% if "replicants" in group_names %}
-    "replicant",
-    {% endif %}
-    {% if "emqx" in group_names %}
-    "emqx",
-    {% endif %}
-  ]
+  id: "{{ inventory_hostname }}:19876"
